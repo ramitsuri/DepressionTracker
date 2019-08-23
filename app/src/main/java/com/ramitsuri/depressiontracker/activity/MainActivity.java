6308d9e316607e99c44cb96ddb2bebd2e4364d0e
ramitsuri <ramitsuri007@gmail.com>
2019-08-23 17:34:42 -0400

    added Google sign in capability with Drive and Sheet scopes

--- app/src/main/java/com/ramitsuri/depressiontracker/activity/MainActivity.java
+++ app/src/main/java/com/ramitsuri/depressiontracker/activity/MainActivity.java
@@ -1,10 +1,19 @@
 package com.ramitsuri.depressiontracker.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.tasks.Task;
 import com.ramitsuri.depressiontracker.R;
+import com.ramitsuri.depressiontracker.constants.Constants;
+import com.ramitsuri.depressiontracker.google.SignInResponse;
+import com.ramitsuri.depressiontracker.google.AccountManager;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +21,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AccountManager accountManager = new AccountManager();
+        SignInResponse response = accountManager.prepareSignIn(this, Constants.SCOPES);
+        if (response.getGoogleSignInAccount() != null) {
+            // do something with account
+        } else if (response.getGoogleSignInIntent() != null) {
+            // request account access
+            startActivityForResult(response.getGoogleSignInIntent(),
+                    Constants.RequestCode.GOOGLE_SIGN_IN);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == Constants.RequestCode.GOOGLE_SIGN_IN) {
+            Task<GoogleSignInAccount> getAccountTask =
+                    GoogleSignIn.getSignedInAccountFromIntent(data);
+            if (getAccountTask.isSuccessful() && getAccountTask.getResult() != null) {
+                // do something with getAccountTask.getResult().getAccount()
+            } else {
+                Timber.i("Sign-in failed.");
+            }
+        }
     }
 }
--- app/src/main/java/com/ramitsuri/depressiontracker/constants/Constants.java
+++ app/src/main/java/com/ramitsuri/depressiontracker/constants/Constants.java
@@ -5,4 +5,8 @@ import com.google.api.services.sheets.v4.SheetsScopes;
 public class Constants {
 
     public static final String[] SCOPES = {SheetsScopes.SPREADSHEETS, SheetsScopes.DRIVE};
+
+    public class RequestCode {
+        public static final int GOOGLE_SIGN_IN = 100;
+    }
 }
