046c64f5c7c1abaa884519b7fb1e8bdc23bac630
ramitsuri <ramitsuri007@gmail.com>
2019-10-03 12:45:35 +0200

    added crashlytics logging for exceptions

--- app/src/main/java/com/ramitsuri/depressiontracker/logging/ReleaseTree.java
+++ app/src/main/java/com/ramitsuri/depressiontracker/logging/ReleaseTree.java
@@ -2,18 +2,31 @@ package com.ramitsuri.depressiontracker.logging;
 
 import android.util.Log;
 
+import com.crashlytics.android.Crashlytics;
+
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import timber.log.Timber;
 
 public class ReleaseTree extends Timber.Tree {
+    private static final String CRASHLYTICS_KEY_PRIORITY = "priority";
+    private static final String CRASHLYTICS_KEY_TAG = "tag";
+    private static final String CRASHLYTICS_KEY_MESSAGE = "message";
 
     @Override
     protected void log(int priority, @Nullable String tag, @NotNull String message,
             @Nullable Throwable t) {
-        if(priority == Log.ERROR || priority == Log.WARN){
-            // TODO Crashlytics
+        if (priority == Log.ERROR || priority == Log.WARN) {
+            Crashlytics.setInt(CRASHLYTICS_KEY_PRIORITY, priority);
+            Crashlytics.setString(CRASHLYTICS_KEY_TAG, tag);
+            Crashlytics.setString(CRASHLYTICS_KEY_MESSAGE, message);
+
+            if (t == null) {
+                Crashlytics.logException(new Exception(message));
+            } else {
+                Crashlytics.logException(t);
+            }
         }
     }
 }
